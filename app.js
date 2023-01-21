1b417506e842990436eada8285bf6f71bee99c09
ahmedfarouk2 <ahmedoza12@gmail.com>
2023-01-22 00:19:12 +0200

    Click sound added

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const allBtns = document.querySelectorAll('.btn');
+const clickSound = document.querySelector('audio')
 
+
+for (let i = 0; i < allBtns.length; i++) {
+    allBtns[i].addEventListener('click', e => {
+        clickSound.currentTime = 0;
+        clickSound.play();
+    })
+}
