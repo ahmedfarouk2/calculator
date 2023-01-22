ba80a600186b6fea73698d9aefd0af70d2d2d755
ahmedfarouk2 <ahmedoza12@gmail.com>
2023-01-22 16:28:52 +0200

    Added JS Logic for deleting and clearing and displaying nums

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const allBtns = document.querySelectorAll('.btn');
-const clickSound = document.querySelector('audio')
+const clickSound = document.querySelector('audio');
+const numberOne = document.querySelector('#one');
+const numberTwo = document.querySelector('#two');
+const numberThree = document.querySelector('#three');
+const numberFour = document.querySelector('#four');
+const numberFive = document.querySelector('#five');
+const numberSix = document.querySelector('#six');
+const numberSeven = document.querySelector('#seven');
+const numberEight = document.querySelector('#eight');
+const multiplySign = document.querySelector('#multiply');
+const divideSign = document.querySelector('#divide');
+const dotSign = document.querySelector('#dot');
+const plusSign = document.querySelector('#plus');
+const substractSign = document.querySelector('#substract');
+const equalsSign = document.querySelector('#equals');
+const resultContainer = document.querySelector('.result-container');
+const clearContainer = document.querySelector('.clear-container');
+const resultDiv = document.querySelector('.hidden-div');
+const mathematicalContainer = document.querySelector('.mathematical-container');
+const deleteContainer = document.querySelector('.delete-container');
+let slicedText;
 
 
 for (let i = 0; i < allBtns.length; i++) {
@@ -8,3 +28,25 @@ for (let i = 0; i < allBtns.length; i++) {
         clickSound.play();
     })
 }
+
+
+mathematicalContainer.addEventListener('click', e => {
+    if (e.target.classList.contains('math') && e.target.innerText !== '='){
+        resultContainer.append(e.target.innerText)
+    }
+})
+
+clearContainer.addEventListener('click', e => {
+    resultContainer.innerText = null;
+})
+
+deleteContainer.addEventListener('click', (e) => {
+    if(resultContainer.innerText.length > 1){
+        slicedText = resultContainer.innerText.slice(0,-1);
+    }
+    else {
+        slicedText = resultContainer.innerText.slice(0,0);    
+    }
+    resultContainer.innerText = slicedText;
+});
+
